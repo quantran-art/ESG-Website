32fa726bb045dd4ed979ac0ffa4f44cd183f346c
quantran-art <quan.tran@erasight.edu.vn>
2025-08-24 21:10:09 +0700

    page.tsx

--- esg-website/app/page.tsx
+++ esg-website/app/page.tsx
@@ -1,15 +1,23 @@
 "use client"
 
 import { useEffect } from "react"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function HomePage() {
-  useEffect(() => {
+  uuseEffect(() => {
+  const timeout = setTimeout(() => {
     window.location.href = "/homepage_esg_intelligence_hub.html"
-  }, [])
+  }, 500) // trì hoãn 0.5 giây
+
+  return () => clearTimeout(timeout)
+}, [])
 
   return (
     <div className="flex items-center justify-center min-h-screen">
       <p>Redirecting to your website...</p>
+      <Analytics />
     </div>
   )
 }
+
+
