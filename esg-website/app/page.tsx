305fb2f1eef047615aa22264b259c35bbb63e4d3
quantran-art <quan.tran@erasight.edu.vn>
2025-09-05 22:39:44 +0700

    page.tsx

--- esg-website/app/page.tsx
+++ esg-website/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 
 export default function HomePage() {
   useEffect(() => {
-    window.location.href = "/homepage_esg_intelligence_hub.html"
+    window.location.href = "/Homepage"
   }, [])
 
   return (
