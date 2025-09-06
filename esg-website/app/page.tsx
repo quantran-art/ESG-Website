b40d447eea89c79ab651a769d21b399fcbbb2b05
quantran-art <quan.tran@erasight.edu.vn>
2025-09-06 10:15:08 +0700

    page.tsx

--- esg-website/app/page.tsx
+++ esg-website/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 
 export default function HomePage() {
   useEffect(() => {
-    window.location.href = "/HomePage"
+    window.location.href = "/Homepage"
   }, [])
 
   return (
